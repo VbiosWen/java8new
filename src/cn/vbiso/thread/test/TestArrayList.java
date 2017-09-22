0a3a90d98ee13d92818e4188ea032d0eac99ed11
VbisoWen <1067477616@qq.com>
2017-09-22 22:08:42 +0800

    Thread-day03

---
+++ src/cn/vbiso/thread/test/TestArrayList.java
@@ -0,0 +1,14 @@
+package cn.vbiso.thread.test;
+
+import java.util.ArrayList;
+
+public class TestArrayList {
+
+    public static void main(String[] args){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(1);
+        System.out.println(arrayList.size());
+    }
+
+}
